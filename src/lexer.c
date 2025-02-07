79cb935120d704a49877ba4c709546f955de65f8
Lawrence D. Kern <law@lawkern.com>
2025-02-06 21:35:39 -0500

    Tokenize integer and float literals

--- src/lexer.c
+++ src/lexer.c
@@ -10,9 +10,9 @@ typedef enum {
 
    TOKEN_COMMA,
    TOKEN_PERIOD,
-   TOKEN_PLUS,
-   TOKEN_MINUS,
    TOKEN_SEMICOLON,
+   TOKEN_MINUS,
+   TOKEN_PLUS,
    TOKEN_SLASH,
    TOKEN_ASTERISK,
 
@@ -62,33 +62,102 @@ typedef struct {
 static int token_count;
 static lexical_token tokens[1024];
 
+static void consume_whitespace(char **code, int *line)
+{
+   while(**code)
+   {
+      char c = **code;
+
+      if(c == ' ' || c == '\t' || c == '\f' || c == '\v')
+      {
+         // NOTE: Skip it.
+      }
+      else if(c == '\n')
+      {
+         (*line)++;
+      }
+      else
+      {
+         break;
+      }
+
+      (*code)++;
+   }
+}
+
+static bool is_digit(char c)
+{
+   return (c >= '0' && c <= '9');
+}
+
 static void lex(memarena *perma, memarena trans, char *code)
 {
    int line = 1;
 
    while(*code)
    {
-      if(*code != ' ')
+      consume_whitespace(&code, &line);
+      if(*code == 0)
       {
-         if(*code == '\n')
-         {
-            line++;
-         }
-         else
-         {
-            lexical_token *token = tokens + token_count++;
-            token->kind = TOKEN_IDENTIFIER;
-            token->lexeme = (string){code, 1};
-            token->line = line;
-         }
+         break;
       }
 
-      code++;
+      lexical_token *token = tokens + token_count++;
+      token->line = line;
+      token->lexeme = (string){code, 1};
+
+      switch(*code)
+      {
+         case ',': {token->kind = TOKEN_COMMA;} break;
+         case '.': {token->kind = TOKEN_PERIOD;} break;
+         case ';': {token->kind = TOKEN_SEMICOLON;} break;
+         case '-': {token->kind = TOKEN_MINUS;} break;
+         case '+': {token->kind = TOKEN_PLUS;} break;
+         case '*': {token->kind = TOKEN_ASTERISK;} break;
+         case '/': {token->kind = TOKEN_SLASH;} break;
+
+         case ':': {token->kind = TOKEN_COLON;} break;
+
+         case '1': case '2': case '3': case '4': case '5':
+         case '6': case '7': case '8': case '9': case '0':
+         {
+            memindex length = 0;
+            while(code[length] && is_digit(code[length]))
+            {
+               length++;
+            }
+
+            if(code[length] == '.')
+            {
+               token->kind = TOKEN_FLOAT;
+               length++;
+
+               while(code[length] && is_digit(code[length]))
+               {
+                  length++;
+               }
+            }
+            else
+            {
+               token->kind = TOKEN_INTEGER;
+            }
+
+            token->lexeme.length = length;
+         } break;
+
+         default: {token->kind = TOKEN_IDENTIFIER;} break;
+      }
+
+      code += token->lexeme.length;
    }
 
    for(int index = 0; index < token_count; ++index)
    {
       lexical_token *token = tokens + index;
-      printf("[Line %d]: %.*s\n", token->line, (int)token->lexeme.length, token->lexeme.data);
+      printf("[TOKEN %2d, Line %d]: %.*s\n",
+             token->kind,
+             token->line,
+             (int)token->lexeme.length,
+             token->lexeme.data);
    }
 }
