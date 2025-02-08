22a7bd4b3a6e3f16a42b3e232000abe45a1f789b
Lawrence D. Kern <law@lawkern.com>
2025-02-07 19:06:40 -0500

    Tokenize multi-character identifiers

--- src/lexer.c
+++ src/lexer.c
@@ -60,7 +60,7 @@ typedef struct {
 } lexical_token;
 
 static int token_count;
-static lexical_token tokens[1024];
+static lexical_token tokens[1 << 24];
 
 static void consume_whitespace(char **code, int *line)
 {
@@ -88,6 +88,14 @@ static void consume_whitespace(char **code, int *line)
 static bool is_digit(char c)
 {
    return (c >= '0' && c <= '9');
+}
+
+static bool is_identifier(char c)
+{
+   return ((c >= '0' && c <= '9') ||
+           (c >= 'A' && c <= 'Z') ||
+           (c >= 'a' && c <= 'z') ||
+           (c == '_'));
 }
 
 static void lex(memarena *perma, memarena trans, char *code)
@@ -118,6 +126,30 @@ static void lex(memarena *perma, memarena trans, char *code)
 
          case ':': {token->kind = TOKEN_COLON;} break;
 
+         case '_':
+         case 'A': case 'B': case 'C': case 'D': case 'E':
+         case 'F': case 'G': case 'H': case 'I': case 'J':
+         case 'K': case 'L': case 'M': case 'N': case 'O':
+         case 'P': case 'Q': case 'R': case 'S': case 'T':
+         case 'U': case 'V': case 'W': case 'X': case 'Y':
+         case 'Z':
+         case 'a': case 'b': case 'c': case 'd': case 'e':
+         case 'f': case 'g': case 'h': case 'i': case 'j':
+         case 'k': case 'l': case 'm': case 'n': case 'o':
+         case 'p': case 'q': case 'r': case 's': case 't':
+         case 'u': case 'v': case 'w': case 'x': case 'y':
+         case 'z':
+         {
+            token->kind = TOKEN_IDENTIFIER;
+            memindex length = 0;
+            while(code[length] && is_identifier(code[length]))
+            {
+               length++;
+            }
+
+            token->lexeme.length = length;
+         } break;
+
          case '1': case '2': case '3': case '4': case '5':
          case '6': case '7': case '8': case '9': case '0':
          {
@@ -145,7 +177,7 @@ static void lex(memarena *perma, memarena trans, char *code)
             token->lexeme.length = length;
          } break;
 
-         default: {token->kind = TOKEN_IDENTIFIER;} break;
+         default: {token->kind = TOKEN_NIL;} break;
       }
 
       code += token->lexeme.length;
